d366ef5be154692a81fbdbad5b02641bc0947e2e
africoiner <confidencenyirenda7@gmail.com>
2022-05-23 20:27:34 +0200

    added functionality to toggle side bar between hide and show

--- sidebar/app.js
+++ sidebar/app.js
@@ -1,5 +1,13 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+const toggleBtn = document.querySelector(".sidebar-toggle")
+const closeBtn = document.querySelector(".close-btn")
+const sidebar = document.querySelector(".sidebar")
+
+toggleBtn.addEventListener('click', function(){
+    // if (sidebar.classList.contains('show-sidebar')){
+    //     sidebar.classList.remove('show-sidebar')
+    // }else {
+    //     sidebar.classList.add('show-sidebar')
+    // }
+    sidebar.classList.toggle('show-sidebar')
+})
+
