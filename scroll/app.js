42aa5b403a2313c4763f307311de894aa9369888
africoiner <confidencenyirenda7@gmail.com>
2022-05-30 17:41:21 +0200

    fixed bugs on smooth scroll positions

--- scroll/app.js
+++ scroll/app.js
@@ -60,11 +60,24 @@ scrollLink.forEach(function(link){
         // navigate to specific spot
         const id = e.currentTarget.getAttribute("href").slice(1)
         const element = document.getElementById(id)
-        let position = element.offsetTop
+        // calculate the height
+        const navHeight = navbar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+        const fixedNav = navbar.classList.contains('fixed-nav')
+        let position = element.offsetTop - navHeight
+
+        if (!fixedNav){
+            position = position - navHeight
+        }
+        if (navHeight > 82){
+            position = position + containerHeight
+        }
+
         window.scrollTo({
             left: 0,
-            top: position
+            top: position,
         })
+        linksContainer.style.height = 0
     })
 })
 
