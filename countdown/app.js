15bf80f1ceb17d38919b8353850fd1b9c1872cf3
africoiner <confidencenyirenda7@gmail.com>
2022-05-31 17:12:33 +0200

    added function to dispay end date

--- countdown/app.js
+++ countdown/app.js
@@ -21,3 +21,25 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway')
+const deadline = document.querySelector('.deadline')
+const items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2022, 10, 17, 11, 30, 0)
+
+const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+
+let month = futureDate.getMonth()
+month = months[month]
+
+const date = futureDate.getDate()
+
+let weekday = futureDate.getDay()
+weekday = weekdays[weekday]
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`
+
+// future time in ms
+
