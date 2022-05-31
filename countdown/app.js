832ebcee3ab9d9956c56eec6cf327f20c2fbd46b
africoiner <confidencenyirenda7@gmail.com>
2022-05-31 17:58:09 +0200

    added count down timer

--- countdown/app.js
+++ countdown/app.js
@@ -43,3 +43,36 @@ giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${h
 
 // future time in ms
 
+const futureTime = futureDate.getTime()
+
+function getRemainingTime(){
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  let days = Math.floor(t / oneDay)
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / 1000)
+  
+  // set values array
+  const values = [days, hours, minutes, seconds]
+
+  function format(item){
+    if (item < 10){
+      return item = `0${item}`
+    }
+    return item
+  }
+
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index])
+  })
+}
+
+let countdown = setInterval(getRemainingTime, 1000)
+
+getRemainingTime()
