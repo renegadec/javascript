06955227c48bd6c757e685898b0febf2f6ae38a6
africoiner <confidencenyirenda7@gmail.com>
2022-06-06 10:33:10 +0200

    added a feature to return the number of paragraphs requested by user

--- lorem/app.js
+++ lorem/app.js
@@ -65,7 +65,14 @@ form.addEventListener('submit', function(e){
   const value = parseInt(amount.value)
   const random = Math.floor(Math.random() * text.length)
   
-  if(isNaN(value) || value < 0 || value > 9) {
+  if(isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`
+  }
+  else{
+    let tempText = text.slice(0,value)
+    tempText = tempText.map(function(item){
+      return `<p class="result">${item}</p>`
+    }).join("")
+    result.innerHTML = tempText
   }
 })
